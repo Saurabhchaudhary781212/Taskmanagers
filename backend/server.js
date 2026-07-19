e325985b36646d6754997b85feea1bf12a7cb1f7
Saurabhchaudhary781212 <saurabhchaudhary4021@gmail.com>
2026-07-19 15:57:17 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ dotenv.config();
 import app from "./src/app.js";
 import connectDB from "./src/db/db.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const startServer = async() => {
     try {
