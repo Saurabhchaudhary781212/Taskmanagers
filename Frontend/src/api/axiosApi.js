7ef70f8a9f414e8a893a715949fbbe88f6ac9b9d
Saurabhchaudhary781212 <saurabhchaudhary4021@gmail.com>
2026-07-19 17:36:35 +0530

    Add root route

--- Frontend/src/api/axiosApi.js
+++ Frontend/src/api/axiosApi.js
@@ -1,19 +1,26 @@
 import axios from "axios";
 
-
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://taskmanagers-ou45.onrender.com/api",
+    baseURL: import.meta.env.VITE_API_URL ||
+        "http://localhost:8000/api"
 });
 
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
+api.interceptors.request.use(
+    (config) => {
+        const token =
+            localStorage.getItem("token");
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+        if (token) {
+            config.headers.Authorization =
+                `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-
-    return config;
-});
+);
 
 export default api;
 
